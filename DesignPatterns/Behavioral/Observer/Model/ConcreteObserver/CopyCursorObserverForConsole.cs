7f5fc528ec3b74326ba2b26bbb66b8fa0ba33921
sbastos29 <sbastos29@gmail.com>
2017-03-18 16:14:10 -0300

    Observer pattern

---
+++ DesignPatterns/Behavioral/Observer/Model/ConcreteObserver/CopyCursorObserverForConsole.cs
@@ -0,0 +1,17 @@
+﻿using DesignPatterns.Behavioral.Observer.Model.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Behavioral.Observer.Model.ConcreteObserver
+{
+    public class CopyCursorObserverForConsole : ICopyCursorObserver
+    {
+        public void UpdatePercentageCursor(int percentage)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DesignPatterns/Behavioral/Observer/Model/CopyProcessor.cs
@@ -0,0 +1,48 @@
+﻿using DesignPatterns.Behavioral.Observer.Model.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Behavioral.Observer
+{
+    public class CopyProcessor
+    {
+        public IList<ICopyCursorObserver> _observerList;
+
+        public CopyProcessor()
+        {
+            _observerList = new List<ICopyCursorObserver>();
+        }
+
+        public void Attach(ICopyCursorObserver observer)
+        {
+            _observerList.Add(observer);
+        }
+
+        public void Detach(ICopyCursorObserver observer)
+        {
+            _observerList.Remove(observer);
+        }
+
+        public void ProcessCopy(string directoryPath)
+        {
+            //Find files
+
+            for(var counter =1; counter <= 100; counter++)
+            {
+                //Copy file
+                NotifyScreen(counter);
+            }
+        }
+
+        private void NotifyScreen(int percentage)
+        {
+            foreach(var observer in _observerList)
+            {
+                observer.UpdatePercentageCursor(percentage);
+            }
+        }
+    }
+}
